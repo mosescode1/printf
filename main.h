086f6d6d1bc9783011b0200eb14cd3f60b5c75fb
mosescode1 <emosescode@gmail.com>
2023-09-16 20:46:16 +0100

    fixed betty

--- main.h
+++ main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-* _printf - function for printing
-* @str: string format
-* Return: strings to be printed
-*/
-int _length_m( char *);
+int _length_m(char *);
 int _mputchar(char mwrite);
 int _printf(const char *format, ...);
 #endif
